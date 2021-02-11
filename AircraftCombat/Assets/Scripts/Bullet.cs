c71ed6bdb3aee51fd9ea1319dcde2b9993e8b766
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:47:47 +0530

    #ADDED : Added bullet script.
    This script is attached to bullets. Currently thers are two types of bullets
    1. Enemy Bullet.
    2. Player Bullet.

---
+++ AircraftCombat/Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AirCraftCombat.CoreData;
+
+namespace AirCraftCombat
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+        [SerializeField] private BulletType bulletType;
+        [SerializeField] private float damageAmount = 5f;
+        private Rigidbody rb;
+        private float startTime = 0f;
+        private float distance = 0f;
+        private float timeInterval = 0f;
+
+        public float Speed { get { return speed; } set { speed = value;} }
+        public float Damage { get { return damageAmount; } }
+        public BulletType Type { get { return bulletType;} set { bulletType = value; } }
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Fire(Vector3 fireDirection)
+        {
+            rb.velocity = fireDirection * speed;
+        }
+
+        public void KillMe()
+        {
+            gameObject.SetActive(false);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
