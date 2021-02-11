b5187e1369cff8466383240b349f3613e67cf470
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 01:12:32 +0530

    #ADDED : Addd soundManager script.
    This is used to play all the sounds releated to game. it is attaced to singleton gameobject.

---
+++ AircraftCombat/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager instance = null;
+
+        [SerializeField] private AudioSource audioSource;
+
+        [SerializeField] private AudioClip explosionClip;
+        [SerializeField] private AudioClip spreadPowerUpClip;
+        [SerializeField] private AudioClip damageClip;
+        [SerializeField] private AudioClip fireBulletsClip;
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void PlayerExplosionSound()
+        {
+            audioSource.PlayOneShot(explosionClip);
+        }
+
+        public void PlayerSpreadPowerUpSound()
+        {
+            audioSource.PlayOneShot(spreadPowerUpClip);
+        }
+
+        public void PlayTakeDamageClip()
+        {
+            audioSource.PlayOneShot(damageClip);
+        }
+
+        public void PlayFireBulletsClip()
+        {
+            audioSource.PlayOneShot(fireBulletsClip);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
