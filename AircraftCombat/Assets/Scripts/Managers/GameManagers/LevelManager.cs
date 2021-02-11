a2920ca16125861ed6b202e30f6e107100498d60
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 01:07:43 +0530

    #ADDED : Added levelManager script
    This scripts is where the gameplay starts, it create enemies and all data is reseted in this script.

---
+++ AircraftCombat/Assets/Scripts/Managers/GameManagers/LevelManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public static LevelManager instance = null;
+        public LevelData levelData;
+
+        [SerializeField] private Player playerBehaviour;
+        public Player Player { get { return playerBehaviour; } }
+
+        #region Untiy Methods
+
+        private void Awake()
+        {
+            instance = this;
+            Config.screenBound = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.y));
+            Utils.Log("LevelManager,Awake, ScreenBound : " + Config.screenBound);
+        }
+
+        private void Start()
+        {
+            Init();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public Vector3 GetPlayerBehaviuorPositon()
+        {
+            return playerBehaviour.transform.position;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Init()
+        {
+            ResetVars();
+            CreateLevel();
+        }
+
+        private void ResetVars()
+        {
+            Config.isGameComplete = false;
+            Config.currentScore = 0;
+        }
+
+        private void CreateLevel()
+        {
+            CreateEnemy();
+        }
+
+        private void CreateEnemy()
+        {
+            EnemySpwaner.instance.StartSpawningEnemies(levelData.levelData[Config.currentLevelIndex]);
+        }
+
+        #endregion
+
+    }
+
+}
