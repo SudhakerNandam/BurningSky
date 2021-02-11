17df1b8f22d73c37835365b58c17341120f0acf7
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:52:20 +0530

    #ADDED : Added Config Script.
    This script contains all the static variable which are used over the complete game.
    Ex : Player Score, Current Level Index, Target Score etc.

---
+++ AircraftCombat/Assets/Scripts/Core/Config.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public class Config
+    {
+        public static int mainmenuSceneIndex = 0;
+        public static int gameplaySceneIndex = 1;
+
+        public static int highestUnlockedLevelIndex = 0;
+        public static int currentLevelIndex = 0;
+        public static Vector3 screenBound = Vector3.zero;
+
+        public static int targetScore = 0;
+        public static int currentScore = 0;
+        public static int highestScore = 0;
+
+        public static int totalLevels = 5;
+
+        public static bool isGameComplete = false;
+    }
+}
