e8a550e9f24fe658895c77d61269a6ffb574f4f5
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:46:24 +0530

    #ADDED : Added BackgroundScrolling script.
    This script does the in game background parallax job.

---
+++ AircraftCombat/Assets/Scripts/BackGroundScrolling.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public class BackGroundScrolling : MonoBehaviour
+    {
+        [SerializeField] private Vector3 startPotion;
+        [SerializeField] private Vector3 endPosition;
+
+        private float speed = 1f;
+        private float endPos = 0f;
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            Mesh m = gameObject.transform.GetChild(0).GetComponent<MeshFilter>().mesh;
+            Bounds b = m.bounds;
+            endPos = b.size.y - 2;
+        }
+
+        void Update()
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            if (transform.position.z <= -endPos)
+                transform.position = startPotion;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
