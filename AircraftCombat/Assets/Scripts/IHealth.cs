4a0346e6a1eee23ebf50c0f7ece38ecc12a799c3
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 01:02:23 +0530

    #ADDED : Added a Ihealth interface script.

---
+++ AircraftCombat/Assets/Scripts/IHealth.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHealth
+{
+    float GetHealth();
+    void TakeDamage(float damageAmount);
+    void SetHealth();
+}
