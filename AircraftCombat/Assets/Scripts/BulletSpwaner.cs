8d8401e2c04a7102ce1c1835f7065440dc7b19a6
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:50:36 +0530

    #ADDED : Added Bullet Spwaner script.
    This script spawns the bullets from the pooled list if it is not active in the scene. Other wise create a new bullet and adds to the pool. To spwan bullet we need to send bullet Type.

---
+++ AircraftCombat/Assets/Scripts/BulletSpwaner.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AirCraftCombat.CoreData;
+
+namespace AirCraftCombat
+{
+    [System.Serializable]
+    public class BulletItem
+    {
+        public GameObject objectToPool;
+        public int amountToPool;
+        public bool shouldExpand;
+        public BulletType type;
+    }
+
+    public class BulletSpwaner : MonoBehaviour
+    {
+        public static BulletSpwaner instance = null;
+
+        public List<BulletItem> itemsToPool;
+        public List<Bullet> pooleditems;
+
+        #region Unity methods
+
+        void Awake()
+        {
+            instance = this;
+        }
+
+        void Start()
+        {
+            pooleditems = new List<Bullet>();
+            foreach (BulletItem item in itemsToPool)
+            {
+                for (int i = 0; i < item.amountToPool; i++)
+                {
+                    pooleditems.Add(GetBulletBehaviour(item.type, item.objectToPool));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public Bullet Spawn(BulletType type)
+        {
+            var items = pooleditems.Where(x => x.Type == type && !x.gameObject.activeInHierarchy);
+
+            if (items.Count() > 0)
+            {
+                return items.First();
+            }
+            else
+            {
+                foreach (BulletItem item in itemsToPool)
+                {
+                    if (item.type == type)
+                    {
+                        if (item.shouldExpand)
+                        {
+                            return GetBulletBehaviour(type, item.objectToPool);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Bullet GetBulletBehaviour(BulletType type, GameObject item)
+        {
+            Bullet behaviour = Instantiate(item, Vector3.zero, Quaternion.identity).GetComponent<Bullet>();
+
+            behaviour.gameObject.SetActive(false);
+            behaviour.Type = type;
+            pooleditems.Add(behaviour);
+            return behaviour;
+        }
+
+        #endregion
+    }
+}
