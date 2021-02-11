4e2b4536bacc33e57edd297c270b7c28f09c78fd
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:55:07 +0530

    #ADDED : Added Keys Script.
    This script conatins all the playerprefs keys.

---
+++ AircraftCombat/Assets/Scripts/Core/Keys.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public static class Keys
+    {
+        public static string KEY_HIGH_SCORE = "KEY_HIGH_SCORE";
+        public static string KEY_HIGHEST_LEVEL_UNLOCKED = "KEY_HIGHEST_LEVEL_UNLOCKED";
+    }
+}
