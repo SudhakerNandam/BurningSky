c4ae4dcd035a23073bbfde8d6ac236a07331be53
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 01:04:48 +0530

    #ADDED : Added boundaries Manager Script.
    This scritp is used to destroy oobjects which go out of boundaries.

---
+++ AircraftCombat/Assets/Scripts/Managers/GameManagers/BoundariesManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AirCraftCombat
+{
+    public class BoundariesManager : MonoBehaviour
+    {
+        #region Unity Methods
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //Utils.Log(other.gameObject.name);
+            var bullet = other.GetComponent<Bullet>();
+
+            bullet?.KillMe();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Privte Methods
+
+        #endregion
+    }
+}
