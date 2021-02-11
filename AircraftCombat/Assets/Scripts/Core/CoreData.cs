c0e1e88378bd6a0af048a731b6dd57f5f9083a5b
SUDHAKERNANDAM\SudhakerNandam <sudhakerraon@gmail.com>
2021-02-12 00:54:00 +0530

    #ADDED : Added Core Data script.
    This script is creatd to declare all the enums which are used over the complete game. This makes easy to access all the enums.

---
+++ AircraftCombat/Assets/Scripts/Core/CoreData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace AirCraftCombat.CoreData
+{
+    public enum BulletType
+    {
+        Player = 0,
+        Enemy = 1
+    }
+
+    public enum PrefabType
+    {
+        Small_1,
+        Small_2,
+        Medium_1,
+        Medium_2,
+        Boss
+    }
+
+    public enum PowerUp
+    {
+        None = -1,
+        Shield = 0,
+        Spread = 1
+    }
+
+    public enum Formation
+    {
+        Type_1 = 0,
+        Type_2 = 1,
+        Type_3 = 2,
+        Type_4 = 3,
+        Type_5 = 4
+    }
+
+}
